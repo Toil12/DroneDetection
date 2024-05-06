45a832cc488143c8ae16f08ad731b4560a104798
Toil12 <femstld@outlook.com>
2024-05-06 16:55:59 +0800

    attach the data access

--- matlab_view.py
+++ matlab_view.py
@@ -1,6 +1,6 @@
 import scipy
 
-annots=scipy.io.loadmat(rf"F:\Data\USC\youtube\image&label-20240421T165405Z-001\image&label\2\anotation")
+annots=scipy.io.loadmat(f"dataset_local/5/anotation.mat")
 # print(annots)
 for key,value in annots.items():
     print(key,value)
