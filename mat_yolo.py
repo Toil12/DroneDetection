2f2a97ad9cd7a01bb7627e41ae464103820cb62f
Toil12 <femstld@outlook.com>
2024-05-15 11:06:19 +0800

    run the demo

--- mat_yolo.py
+++ mat_yolo.py
@@ -8,6 +8,8 @@ import scipy
 import numpy as np
 import os
 
+from PIL import Image
+
 # The keys of matlab file is as the followed, box is the item including data:
 # __header__
 # __version__
@@ -17,20 +19,26 @@ annotations = scipy.io.loadmat(f"datasets/usc_5/annotations/anotation.mat")
 
 # Get the file name for the label file process
 images_training=[x.split('.')[0] for x in os.listdir(f"datasets/usc_5/images/train")]
+image_example=Image.open(f'datasets/usc_5/images/train/{os.listdir(f"datasets/usc_5/images/train")[0]}')
+image_example=np.array(image_example)
+h,w,c=image_example.shape
 
 # Each data is with 4 features, x_center, y_center, width, height in pixels
 labels = annotations['box']
 labels = np.array(labels)
 # print(labels[0:5])
-print(type(labels[0][0]))
+# print(type(labels[0][0]))
 
 labels_training_path=f"datasets/usc_5/labels/train"
 
 for i in range(len(labels)):
     file_name=str((i+1)*10)
     with open(f'{labels_training_path}/{file_name}.txt', mode='w') as f:
-        for item in labels[i]:
-            f.write(str(item)+' ')
+        f.write("0"+' ')
+        f.write(str(labels[i][0] / w) + ' ')
+        f.write(str(labels[i][1] / h) + ' ')
+        f.write(str(labels[i][2] / w) + ' ')
+        f.write(str(labels[i][3] / h) + ' ')
 
 
 
--- test3.py
+++ test3.py
@@ -10,7 +10,7 @@ import torch
 model = torch.hub.load("ultralytics/yolov5", "yolov5s")  # Can be 'yolov5n' - 'yolov5x6', or 'custom'
 
 # Inference on images
-img = "https://ultralytics.com/images/zidane.jpg"  # Can be a file, Path, PIL, OpenCV, numpy, or list of images
+img = f"./datasets/usc_5"  # Can be a file, Path, PIL, OpenCV, numpy, or list of images
 
 # Run inference
 results = model(img)
