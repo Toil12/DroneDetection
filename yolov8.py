9c50784adc3535ba8ef6a36896ea98b75b34170c
Toil12 <femstld@outlook.com>
2024-05-27 18:46:27 +0800

    yolov8 works

---
+++ yolov8.py
@@ -0,0 +1,16 @@
+"""
+coding:utf-8
+@Time       :2024/5/27 8:33
+@Author     :ywLi
+@Institute  :DonghaiLab
+"""
+from ultralytics import YOLO
+
+# Load a model
+model = YOLO("yolov8n.pt")  # load a pretrained model (recommended for training)
+
+# Train the model with 2 GPUs
+results = model.train(data=f"yolov5-master/data/usc_5.yaml",
+                      epochs=100,
+                      imgsz=1280,
+                      device='cpu')
