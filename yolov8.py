a74b49d1880d634dd16c04ec17533a7c7e07385a
Toil12 <femstld@outlook.com>
2024-05-29 14:02:28 +0800

    yolov8 train

--- yolov8.py
+++ yolov8.py
@@ -7,10 +7,11 @@ coding:utf-8
 from ultralytics import YOLO
 
 # Load a model
-model = YOLO("yolov8n.pt")  # load a pretrained model (recommended for training)
+model = YOLO("yolov8s.pt")  # load a pretrained model (recommended for training)
 
 # Train the model with 2 GPUs
 results = model.train(data=f"yolov5-master/data/usc_5.yaml",
                       epochs=100,
                       imgsz=1280,
-                      device=[0,1])
+                      device=[0,1],
+                      optimizer='Adam')
