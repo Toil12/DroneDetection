63bd3125d843291807c6128bf61c197a44943300
Toil12 <femstld@outlook.com>
2024-06-21 18:13:16 +0800

    yolov8 train

--- yolov8.py
+++ yolov8.py
@@ -4,13 +4,19 @@ coding:utf-8
 @Author     :ywLi
 @Institute  :DonghaiLab
 """
+import argparse
+
 from ultralytics import YOLO
 import pathlib
 import os
 import torch
 
 if __name__ == '__main__':
+    parser=argparse.ArgumentParser()
+
     datapath=pathlib.Path(os.path.curdir,"usc_5_v8.yaml")
+    testpath=pathlib.Path(os.path.curdir,"")
+
     devices_count=torch.cuda.device_count()
     devices=[x for x in range(devices_count)]
     # print(devices)
@@ -24,4 +30,15 @@ if __name__ == '__main__':
                           imgsz=1280,
                           device=devices,
                           optimizer='Adam')
+    results = model(test_data_path)  # return a list of Results objects
+
+    # Process results list
+    for result in results:
+        boxes = result.boxes  # Boxes object for bounding box outputs
+        masks = result.masks  # Masks object for segmentation masks outputs
+        keypoints = result.keypoints  # Keypoints object for pose outputs
+        probs = result.probs  # Probs object for classification outputs
+        obb = result.obb  # Oriented boxes object for OBB outputs
+        result.show()  # display to screen
+        result.save(filename="result.jpg")  # save to disk
     model.export()
